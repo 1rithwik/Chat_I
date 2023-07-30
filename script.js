34d13d294ff3ff23b7ee415f6aa392cb2073bdb9
1rithwik <93206638+1rithwik@users.noreply.github.com>
2023-07-30 21:22:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,28 @@ const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
-let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const targetDiv = document.getElementById("hide");
+const para=document.querySelector(".para");
+const para1=document.querySelector(".para1");
+const btn = document.getElementById("btn");
+btn.onclick = function () {
+    if (targetDiv.style.background !== "none") {
+      targetDiv.style.background="none";
+      para.style.display="none";
+      para1.style.display="none";
+    } else {
+      targetDiv.style.background="linear-gradient(100deg, #ff220084, #e81d1d7a, #e8b91d8a, #e5e81d84, #1de83f82, #1ddee88f, #2b1de894, #df00f389, #df00f385)";
+      para.style.display="inline-flex";
+      para1.style.display="inline-flex";
+    }
+  };
+
+let userMessage = null; // Variable to store
+const API_KEY = "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";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
-    // Create a chat <li> element with passed message and className
+    // Creating a chat <li> element with passed message and className
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", `${className}`);
     let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
@@ -22,7 +38,7 @@ const generateResponse = (chatElement) => {
     const API_URL = "https://api.openai.com/v1/chat/completions";
     const messageElement = chatElement.querySelector("p");
 
-    // Define the properties and message for the API request
+    //properties and message for the API request
     const requestOptions = {
         method: "POST",
         headers: {
@@ -35,7 +51,7 @@ const generateResponse = (chatElement) => {
         })
     }
 
-    // Send POST request to API, get response and set the reponse as paragraph text
+    //POST request to API, get response and set the reponse as paragraph text
     fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
         messageElement.textContent = data.choices[0].message.content.trim();
     }).catch(() => {
@@ -48,16 +64,15 @@ const handleChat = () => {
     userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
     if(!userMessage) return;
 
-    // Clear the input textarea and set its height to default
+    // Clearing the input textarea and seting its height to default
     chatInput.value = "";
     chatInput.style.height = `${inputInitHeight}px`;
 
-    // Append the user's message to the chatbox
+    // Appending the user's message to the chatbox
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
     
     setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
         const incomingChatLi = createChatLi("Thinking...", "incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
@@ -66,14 +81,11 @@ const handleChat = () => {
 }
 
 chatInput.addEventListener("input", () => {
-    // Adjust the height of the input textarea based on its content
     chatInput.style.height = `${inputInitHeight}px`;
     chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
 
 chatInput.addEventListener("keydown", (e) => {
-    // If Enter key is pressed without Shift key and the window 
-    // width is greater than 800px, handle the chat
     if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
         e.preventDefault();
         handleChat();
